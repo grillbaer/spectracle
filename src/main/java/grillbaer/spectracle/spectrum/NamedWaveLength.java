9fb42c5bdd32dabcdbf72d692b968782fb3e9008
Holger Fleischmann <holgflei@gmail.com>
2021-05-20 21:06:26 +0200

    added Fraunhofer lines to known wavelengths

--- src/main/java/grillbaer/spectracle/spectrum/NamedWaveLength.java
+++ src/main/java/grillbaer/spectracle/spectrum/NamedWaveLength.java
@@ -16,7 +16,19 @@ public class NamedWaveLength {
                     new NamedWaveLength("FL Hg Purple", 405),
                     new NamedWaveLength("FL Hg Bright Blue", 436),
                     new NamedWaveLength("FL Hg Brightest Green", 546),
-                    new NamedWaveLength("FL Eu Bright Red-Orange", 612)
+                    new NamedWaveLength("FL Eu Bright Red-Orange", 612),
+                    new NamedWaveLength("Fraunhofer K Ca+", 393),
+                    new NamedWaveLength("Fraunhofer H Ca+", 397),
+                    new NamedWaveLength("Fraunhofer G Ca/Fe", 431),
+                    new NamedWaveLength("Fraunhofer F H", 486),
+                    new NamedWaveLength("Fraunhofer b2 Mg", 517),
+                    new NamedWaveLength("Fraunhofer b1 Mg", 518),
+                    new NamedWaveLength("Fraunhofer E Fe", 527),
+                    new NamedWaveLength("Fraunhofer D2 Na", 589),
+                    new NamedWaveLength("Fraunhofer D1 Na", 590),
+                    new NamedWaveLength("Fraunhofer C H", 656),
+                    new NamedWaveLength("Fraunhofer B O₂", 687),
+                    new NamedWaveLength("Fraunhofer A O₂", 759)
             );
 
     public String getWaveLengthNameString(boolean withUnit) {
