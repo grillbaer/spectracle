e66e3f01e3d0fb0be571b3dd39a2f95a2f3c549d
Holger Fleischmann <holgflei@gmail.com>
2021-06-03 21:42:30 +0200

    added icons for window and application

--- spectracle-app/src/main/java/grillbaer/spectracle/Main.java
+++ spectracle-app/src/main/java/grillbaer/spectracle/Main.java
@@ -5,9 +5,13 @@ import grillbaer.spectracle.camera.Camera;
 import grillbaer.spectracle.ui.MainPanel;
 import nu.pattern.OpenCV;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Application entry point.
@@ -27,11 +31,25 @@ public class Main {
             FlatDarkLaf.install();
             final var mainPanel = new MainPanel(context);
             final var frame = new JFrame("Spectracle");
+            frame.setIconImages(getWindowIcons());
             frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
             frame.getContentPane().add(mainPanel.getComponent(), BorderLayout.CENTER);
             setDefaultWindowBounds(frame);
             frame.setVisible(true);
         });
+    }
+
+    private static List<? extends Image> getWindowIcons() {
+        return List.of(16, 24, 32, 48, 64, 128, 256)
+                .stream()
+                .map(size -> {
+                    try {
+                        return ImageIO.read(
+                                Main.class.getClassLoader().getResource("icons/icon-" + size + ".png"));
+                    } catch (IOException e) {
+                        throw new IllegalStateException("missing window icon", e);
+                    }
+                }).collect(Collectors.toList());
     }
 
     private static void setDefaultWindowBounds(JFrame frame) {
